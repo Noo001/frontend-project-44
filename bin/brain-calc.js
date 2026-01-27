19bc032694873656e952dc23515b5cdc0492d2fb
Andrey <noo_@bk.ru>
2026-01-27 05:47:29 +0300

    + 2 games and index

---
+++ bin/brain-calc.js
@@ -0,0 +1,19 @@
+import * as index from '../src/index.js'
+
+index.intro('What is the result of the expression?')
+
+const operator = () => {
+  switch (Math.floor(Math.random() * 3)) {
+    case 0: return '+'
+    case 1: return '-'
+    case 2: return '*'
+  }
+}
+
+let questions = [], correctAnswers = []
+for (let i = 0; i < index.MAX_ATTEMPTS; i++) {
+  questions[i] = `${Math.floor(Math.random() * 100)} ${operator()} ${Math.floor(Math.random() * 100)}`
+  correctAnswers[i] = eval(questions[i]).toString()
+}
+
+index.app(questions, correctAnswers)
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { welcome } from './cli.js'
+import readlineSync from 'readline-sync'
+
+export const MAX_ATTEMPTS = 3
+const name = welcome()
+
+export function intro(rules) {
+  console.log(rules)
+}
+
+export function app(questions, correctAnswers) {
+  for (let i = 0; i < MAX_ATTEMPTS; i++) {
+    console.log(`Question: ${questions[i]}`)
+    const answer = readlineSync.question('Your answer: ')
+    if (answer === correctAnswers[i]) {
+      console.log('Correct!')
+    }
+    else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswers[i]}.`)
+      console.log(`Let's try again, ${name}!`)
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`)
+}
