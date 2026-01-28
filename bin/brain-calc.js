25ff0e3d2b33fb25b348cd926822273165f29d71
Andrey <noo_@bk.ru>
2026-01-28 09:13:42 +0300

    Шебанг

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { calc } from '../src/games/brain-calc.js'
 
 calc()
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { prime } from '../src/games/brain-prime.js'
 
 prime()
